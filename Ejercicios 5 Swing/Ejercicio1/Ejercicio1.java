739e7ca68da6ca167b3ef34a295780016ee8a5e7
Iker <162556194+IkerOwO@users.noreply.github.com>
2025-09-01 17:56:51 +0200

    Ejercicio 5 Swing

---
+++ Ejercicios 5 Swing/Ejercicio1/Ejercicio1.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Ejercicio1{
+    public static void main(String[] args){
+        Interfaz interfaz = new Interfaz();
+        interfaz.setTitle("BotonesTexto");
+        interfaz.setSize(500,250);
+        interfaz.setVisible(true);
+        interfaz.setResizable(false);
+        interfaz.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+}
